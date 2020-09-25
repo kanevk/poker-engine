ae5696203a352ab95b08795efffdb98e921ebf79
kanevk <kamen.e.kanev@gmail.com>
2020-09-25 17:38:22 +0300

    Remove Pusher
    We've reconsidered the use of Pusher. Using active channel is simpler
    and due to the Poker Engine architecture is horizontally scalable

--- api/config/initializers/pusher.rb
+++
@@ -1,9 +0,0 @@
-PUSHER = Pusher::Client.new(
-  app_id: '1075445',
-  key: '058b4766a9814309c004',
-  secret: '***REMOVED***',
-  cluster: 'eu',
-  encrypted: true
-)
-
-Pusher.logger = Rails.logger
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   end
   post "/graphql", to: "graphql#execute"
   post "/", to: "graphql#execute"
-  post '/pusher/auth', to: 'pusher#auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount ActionCable.server, at: '/cable'
 end
